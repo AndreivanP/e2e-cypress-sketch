43716d9afcbcedd8b12f9c7324a02ff3a91bf15e
Andreivan <andreivan.santos@gmail.com>
2022-06-03 18:59:26 -0300

    ci: Add docker files && GitHub Actions workflow

--- Properties.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://api-asset-management-stg.herokuapp.com'
-export const BUILD_NUMBER = 'buildplaceholder'
-export const MONGOURI = 'mongodb+srv://Andreivan:zaq123@asset-app-prod.w8mhh.mongodb.net/pat_manag_stg'
